9f53ea9c62ca2d58eb690165f2944910f4b74906
Andrey Sytnik <47322954+Ilvif666@users.noreply.github.com>
2019-05-18 15:18:30 +0300

    solved last question of first task

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import numpy as np
 from math import cos,pi
 
 def I(x):# анализируемая функция
-    lambd, d, L, I0 = 0.65E-6, 0.2E-3, 1.3E0, 10
     return 4*I0*(cos(pi*x*d/(L*lambd)))**2
-
+lambd, d, L, I0 = 0.65E-6, 0.2E-3, 1.3E0, 10
 x = np.arange(-2E-2,2E-2,0.1E-3)
 y = [I(each) for each in x]
 yy = np.arange(0,4*10+1,1)
@@ -19,5 +18,6 @@ plt.xlabel("x, м")
 plt.ylabel("I, интенсивность")
 plt.grid(True)
 plt.yticks(range(len(yDic)),list(yDic.keys()))
-
+delta = lambd*L/d
+print(round(delta,6))
 plt.show()
